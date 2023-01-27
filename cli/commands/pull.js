d70604530f8197c8fc0c20e688d2dad82dbf6af0
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-01-27 16:37:51 -0700

    making dir if not exists on pull

--- cli/commands/pull.js
+++ cli/commands/pull.js
@@ -15,6 +15,10 @@ const THEME_PATH = path.resolve(process.cwd(), './theme')
 const pull = program.command('pull')
   .description("Replaces your local theme files with a selected theme's remote theme files.")
   .action(async () => {
+
+    if (!fs.existsSync(THEME_PATH)){
+      fs.mkdirSync(THEME_PATH);
+    }
     const themes = await list().then(res => res.themes)
 
     const choices = themes.map(theme => {
