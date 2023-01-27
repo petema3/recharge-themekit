e28c1087194ec0b237657d1df2501ef01aa994c6
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-01-27 16:02:43 -0700

    watch command

--- cli/commands/watch.js
+++ cli/commands/watch.js
@@ -4,62 +4,89 @@ import path from 'path'
 import fs from 'fs'
 
 import chokidar from 'chokidar';
-import cache from '../../services/cache/index.js'
 
-import edit from '../../services/recharge/theme/edit.js'
+import deleteFile from '../../services/recharge/theme/deleteFile.js';
+import inquirer from 'inquirer';
+import getFiles from '../../services/recharge/theme/getFiles.js';
+import editFile from '../../services/recharge/theme/editFile.js';
+import list from '../../services/recharge/theme/list.js';
+import chalk from 'chalk'
+import newFile from '../../services/recharge/theme/newFile.js';
 
-const validateTheme = () => {
-  // will probably need this when implementing added/removed file functionality
-}
+const THEME_PATH = path.resolve(process.cwd(), './theme')
 
-const handleChangedFile = async (filePath) => {
+let watchedThemeFiles = []
+
+const handleChangedFile = async (filePath, watchedTheme) => {
+  const ui = new inquirer.ui.BottomBar();
   const fileName = path.basename(filePath)
-  const cacheCopy = cache.get()
-
-  const currentTheme = cacheCopy.find(theme => theme.latest === true)
-  const assetIdOfChangedFile = currentTheme.assets.find(asset => asset.name === fileName).id
   const source = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' });
 
-  await edit(currentTheme.id, assetIdOfChangedFile, { source }).then(() => {
-    console.log(`Successfully updated ${fileName}`)
-  })
+  const fileToUpdate = watchedThemeFiles.find(file => file.name === fileName).id
+  await editFile(watchedTheme.id, fileToUpdate, source)
+  ui.log.write(`> Updated ${chalk.cyan(fileName)}`)
 }
 
-const handleAddedFile = () => {
-  //Find added file
-  //upload to recharge
-  //get asset id
-  //update asset_map
+const handleAddedFile = async (filePath, watchedTheme) => {
+  const ui = new inquirer.ui.BottomBar();
+  const fileName = path.basename(filePath)
+  const source = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' });
+  await newFile(watchedTheme.id, fileName, source)
+  watchedThemeFiles = await getFiles(watchedTheme.id)
+  ui.log.write(`> Added ${chalk.cyan(fileName)}`)
 }
 
-const handleRemovedFile = () => {
-  //Find removed file
-  //delete from recharge
-  //get asset id
-  //update asset_map
+const handleRemovedFile = async (filePath, watchedTheme) => {
+  const ui = new inquirer.ui.BottomBar();
+  const fileName = path.basename(filePath)
+  const fileToDelete = watchedThemeFiles.find(file => file.name === fileName).id
+  await deleteFile(watchedTheme.id, fileToDelete)
+  watchedThemeFiles = await getFiles(watchedTheme.id)
+  ui.log.write(`> Deleted ${chalk.cyan(fileName)}`)
 }
 
-const handleReady = () => {
-  const currentTheme = cache.get().find(theme => theme.latest)
-  console.log(`Initial scan complete. Watching for changes for theme: ${currentTheme.name}`)
+const handleReady = (watchedTheme) => {
+  const ui = new inquirer.ui.BottomBar();
+  ui.log.write(`> Now listening for changes to be uploaded to: ${chalk.cyan(watchedTheme.name)}`)
 }
 
 const watch = program.command('watch')
   .description('Watch a directory and upload all file changes to recharge')
   .action(async () => {
-    const THEME_PATH = path.resolve(process.cwd(), './theme')// TODO: get this path dynamically
+    const themes = await list().then(res => res.themes)
 
-    const watcher = chokidar.watch(THEME_PATH, {
-      ignored: /(^|[\/\\])\../, // ignore dotfiles
-      persistent: true,
-      ignoreInitial: true
-    });
+    const choices = themes.map(theme => {
+      return {
+        name: theme.name,
+        value: theme
+      }
+    })
 
-    watcher
-      .on('add', path => handleAddedFile(path))
-      .on('change', path => handleChangedFile(path))
-      .on('unlink', path => handleRemovedFile(path))
-      .on('ready', handleReady);
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'theme',
+          message: 'Please select the theme you would like to upload to during watch:',
+          choices
+        }
+      ])
+      .then( async (answers) => {
+        const watchedTheme = answers.theme
+        watchedThemeFiles = await getFiles(answers.theme.id)
+
+        const watcher = chokidar.watch(THEME_PATH, {
+          ignored: /(^|[\/\\])\../, // ignore dotfiles
+          persistent: true,
+          ignoreInitial: true
+        });
+
+        watcher
+          .on('add', filePath => handleAddedFile(filePath, watchedTheme))
+          .on('change', filePath => handleChangedFile(filePath, watchedTheme))
+          .on('unlink', filePath => handleRemovedFile(filePath, watchedTheme))
+          .on('ready', () => handleReady(watchedTheme));
+      })
   });
 
 export default watch
