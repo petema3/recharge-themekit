c218a062e12a40dacde85b85dadb1be9fe545b72
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-01-27 15:36:42 -0700

    delete file

--- services/recharge/theme/deleteFile.js
+++ services/recharge/theme/deleteFile.js
@@ -1,7 +1,7 @@
 import client from "../../client.js"
 
 const deleteFile = async (themeId, assetId) => {
-  await client.delete(`/portal_theme/${themeId}/assets/${assetId}/delete`)
+  await client.delete(`/portal_theme/${themeId}/assets/${assetId}/delete.json`)
 }
 
 export default deleteFile
