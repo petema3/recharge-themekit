c893791c1c2d2f93d1e56c87bd39f7f698906efc
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-01-27 03:07:01 -0700

    auth command

--- cli/commands/auth.js
+++ cli/commands/auth.js
@@ -1,10 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { Command } from 'commander';
 const program = new Command();
+
+import open from 'open'
 
 const auth = program.command('auth')
   .description('Opens a browser tab to the RECHARGE_APP_URL defined in your .env file to retrieve a new session token')
   .action(async () => {
-
+    open(`${process.env.RECHARGE_APP_URL}/merchant/home`)
   });
 
 export default auth
