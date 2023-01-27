fcf508e542d1302c6112edb8427e0a09bbe8901d
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-01-27 11:10:17 -0700

    package command

--- cli/commands/package.js
+++ cli/commands/package.js
@@ -1,10 +1,39 @@
 import { Command } from 'commander';
 const program = new Command();
 
+import list from '../../services/recharge/theme/list.js';
+import inquirer from 'inquirer';
+import zipper from 'zip-local';
+const ui = new inquirer.ui.BottomBar();
+
 const packageTheme = program.command('package')
   .description("Zips your theme directory and places it in the project's root directory.")
   .action(async () => {
 
+    const themes = await list().then(res => res.themes)
+
+    const choices = themes.map(theme => {
+      return {
+        name: theme.name,
+        value: theme
+      }
+    })
+
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'theme',
+          message: 'Please select the theme you would like to zip:',
+          choices
+        }
+      ])
+      .then( async (answers) => {
+        ui.log.write(`Zipping theme: "${answers.theme.name}"...`)
+        zipper.sync.zip(`${process.cwd()}/theme/`).compress().save("theme.zip");
+        ui.log.write(`Done! Zipped "${answers.theme.name}"`);
+        process.exit()
+      })
   });
 
 export default packageTheme
